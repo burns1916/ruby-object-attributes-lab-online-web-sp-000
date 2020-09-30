457629f7897bef301bdb518cdaf711791611e6ee
Ryan K Burns <ryankburns@gmail.com>
2020-09-30 04:00:34 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+  def initialize(dog_name, dog_breed)
+    @dog_name = dog_name
+    @dog_breed = dog_breed
+  end
+
+  def name=(name)
+    @dog_name = name
+  end
+
+  def name
+    puts @dog_name
+end
